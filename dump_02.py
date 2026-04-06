d2527c642be984aac459826e1699aa8ceef6910f
PLWEP <permana.putra@ifssi.co.id>
2026-04-06 21:21:37 +0700

    refactor: remove legacy patch scripts and initialize new model training notebooks

--- dump_02.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('dump_02.txt', 'w', encoding='utf-8') as out:
-    data = json.load(open('notebooks/02_signal_inference.ipynb', encoding='utf-8'))
-    for i, c in enumerate(data['cells']):
-        if c['cell_type'] == 'code':
-            out.write(f"=== CELL {i} ===\n")
-            out.write(''.join(c['source']))
-            out.write("\n---\n")
